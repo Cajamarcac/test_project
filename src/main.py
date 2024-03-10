e33abd9dd93090e19ca203beb57ea3f2039cb9f3
Cajamarcac <fabiocajamarcaoficial@gmail.com>
2024-03-10 01:36:55 -0500

    se realizan cambios mas complejos a main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
-#Prueba simple
+# se realizan cambios mas complejos a main.py
+class Calculator:
+    def add_numbers(self, a, b):
+        return a + b
 
-def add_numbers(a, b):
-    return a + b
+def main():
+    calculator = Calculator()
+    result = calculator.add_numbers(2, 3)
+    print(result)
+
+if __name__ == '__main__':
+    main()
